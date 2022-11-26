e047d470382a5c97f404d323d4f197b46e283fa1
Daniel <kouznetsov1996@gmail.com>
2022-11-26 12:08:34 +0100

    images??

--- components/Portfolio/portfolioData.ts
+++ components/Portfolio/portfolioData.ts
@@ -3,14 +3,14 @@ export const portfolioData = [
     title: "Personal website",
     description:
       "This website! I made this website to showcase my projects and to learn more about web development.",
-    image: "/website.png",
+    image: "website.png",
     tools: ["React", "Next.js", "TypeScript", "Tailwind CSS"],
     url: "https://github.com/kouznetsov1/personal_website",
   },
   {
     title: "Studies",
     description: "Github repository for some of my studies.",
-    image: "/git.png",
+    image: "git.png",
     tools: [],
     url: "https://github.com/kouznetsov1/studies",
   },
@@ -18,7 +18,7 @@ export const portfolioData = [
     title: "Gainoteket",
     description:
       "Project I am currently working on trying to make it easier for people to plan their meals.",
-    image: "/gainoteket.png",
+    image: "gainoteket.png",
     tools: ["React", "Next.js", "TypeScript", "Tailwind CSS", "MongoDB", "Go"],
     url: "https://github.com/kouznetsov1/prooly",
   },
@@ -26,7 +26,7 @@ export const portfolioData = [
     title: "Course planner",
     description:
       "First web development project I made. It is a course planner for engineering master students at LiU, not finished.",
-    image: "/course_planner.png",
+    image: "course_planner.png",
     tools: [
       "React",
       "JavaScript",
