5cf786e88b542996d9fd232628fdcb568e00c8fd
Daniel <kouznetsov1996@gmail.com>
2023-03-23 19:59:18 +0100

    better responsiveness

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -8,13 +8,10 @@ export const Profile = () => {
   const githubURL = "https://www.github.com/kouznetsov1";
 
   return (
-    <div
-      className="min-h-screen bg-gradient-to-tr animate-gradient from-neutral-900 to-neutral-800"
-      style={{ backgroundSize: "400% 400%" }}
-    >
-      <div className="lg:w-1/2 w-4/5 text-neutral-100 m-auto text-center justify-center">
-        <div className="drop-shadow-lg">
-          <div className="pt-16">
+    <div className="min-h-screen bg-gradient-to-tr animate-gradient from-neutral-900 to-neutral-800">
+      <div className="lg:w-1/2 w-4/5 text-neutral-100 m-auto text-center min-h-screen flex flex-col flex-grow justify-center">
+        <div className="flex flex-col flex-grow justify-evenly">
+          <div className="drop-shadow-lg">
             <div className="flex m-auto justify-center">
               <div className="-mr-32">
                 <a href="https://www.linkedin.com/in/daniel-kouznetsov-489011127/">
@@ -46,25 +43,23 @@ export const Profile = () => {
               </div>
             </div>
           </div>
-          <div className="">
-            <h1 className="text-6xl lg:text-8xl font-extrabold m-6 mt-20">
-              Hello!
-            </h1>
-            <h2 className="text-2xl lg:text-4xl m-6 font-medium">
+          <div className="flex flex-col justify-between">
+            <h1 className="text-6xl lg:text-8xl font-extrabold">Hello!</h1>
+            <h2 className="text-2xl lg:text-4xl font-medium mt-6">
               My name is Daniel Kouznetsov.
             </h2>
-            <p className="lg:text-lg m-6">{aboutMe}</p>
+            <p className="lg:text-lg mt-4">{aboutMe}</p>
+            <div className="mt-12">
+              <h1>Check out what I'm up to down below!</h1>
+            </div>
           </div>
-          <div className="mt-14 lg:mt-24">
-            <h1>Check out what I'm up to down below!</h1>
-          </div>
-          <div className="m-14">
-            <a href="#portfolio">
-              <div className="animate-bounce w-1/2 m-auto">
-                <ArrowDownwardIcon fontSize="large" />
-              </div>
-            </a>
-          </div>
+        </div>
+        <div className="mt-auto">
+          <a href="#portfolio">
+            <div className="animate-bounce">
+              <ArrowDownwardIcon fontSize="large" />
+            </div>
+          </a>
         </div>
       </div>
     </div>
